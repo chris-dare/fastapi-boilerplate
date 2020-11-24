deb12d47b5a2b5c529559343dbadb4cdd88abfd5
Chris Dare <engineering@chrisdare.me>
2020-11-24 12:33:08 +0200

    Add base api_router for v1 package
    Serves as contact point outside of the v1 package

---
+++ app/api/v1/api.py
@@ -0,0 +1,10 @@
+"""
+The :mod:`app.api.v1.api` module sets up, 
+aggregates and consolidates api routes for the version 1 of the api application.
+"""
+# Author: Chris Dare
+# License: 
+ 
+from fastapi import APIRouter
+
+api_router = APIRouter()
